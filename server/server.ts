9ddebe50068d57fad4be0851431594c753d8e98f
Asus Laptop <minhnle311@gmail.com>
2023-06-22 10:46:49 -0700

    Added sales and purchases routes

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 require('dotenv').config({path:'../.env'})
 
 import express from 'express'
+
 const app = express()
 const port = 3001
 
@@ -10,11 +11,15 @@ import productRoute from './api/product/productRoutes'
 import purchaseRoute from './api/purchase/purchaseRoutes'
 import inventoryRoute from './api/inventory/inventoryRoutes'
 import customerRoute from './api/customer/customerRoutes'
+import saleRoute from './api/sale/saleRoutes'
+import supplierRoute from './api/supplier/supplierRoutes'
 
 app.use('/api/v1/products', productRoute)
 app.use('/api/v1/purchases', purchaseRoute)
 app.use('/api/v1/inventory', inventoryRoute)
 app.use('/api/v1/customers', customerRoute)
+app.use('/api/v1/sales', saleRoute)
+app.use('/api/v1/suppliers', supplierRoute)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
