edc2fd489de0f481067ceca94d9ff3c88ece69ad
Asus Laptop <minhnle311@gmail.com>
2023-06-22 10:43:31 -0700

    Fixed query

--- server/api/purchase/purchaseQueries.ts
+++ server/api/purchase/purchaseQueries.ts
@@ -1,4 +1,19 @@
-export const get_all_purchases = 'SELECT * FROM purchases ORDER BY purchase_date DESC LIMIT $1 OFFSET $2'
+export const get_all_purchases = `SELECT 
+                                    purchases.purchase_date,
+                                    products.name,
+                                    purchases.quantity,
+                                    stores.store_name,
+                                    suppliers.name as supplier,
+                                    staffs.first_name || ' ' || staffs.last_name as staff,
+                                    products.id as id
+                                FROM purchases
+                                    INNER JOIN products ON purchases.item = products.id
+                                    INNER JOIN stores ON purchases.store = stores.id
+                                    INNER JOIN suppliers ON purchases.supplier = suppliers.id
+                                    INNER JOIN staffs ON purchases.staff = staffs.id
+
+                                    ORDER BY purchase_date DESC 
+                                    LIMIT $1 OFFSET $2`
 export const purchase_detail = 'SELECT * FROM purchases WHERE id = $1'
 export const purchase_create = 'INSERT INTO purchases (item, quantity, purchase_date, store, supplier, staff) values ($1, $2, $3, $4, $5, $6)'
 export const purchase_update = ''
