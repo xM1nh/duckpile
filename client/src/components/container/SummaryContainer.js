eaf6362c47d6e231b23536613fef407f82807a72
Asus Laptop <minhnle311@gmail.com>
2023-06-23 10:25:23 -0700

    Removed button

--- client/src/components/container/SummaryContainer.js
+++ client/src/components/container/SummaryContainer.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import './_SummaryContainer.css'
 
-const SummaryContainer = ({data_to_show, text_to_show, isLoading, serverErr}) => {
+const SummaryContainer = ({id, data_to_show, text_to_show, isLoading, serverErr}) => {
 
     if (isLoading) {
         return (<div className="container-loading">Loading</div>)
@@ -13,14 +13,9 @@ const SummaryContainer = ({data_to_show, text_to_show, isLoading, serverErr}) =>
 
     else {
         return (
-            <div className="container summary success">
+            <div className="container summary success" id={id}>
                 <div className="container-data value summary">{data_to_show}</div>
                 <div className="container-data key summary">{text_to_show}</div>
-                <Link to=''>
-                    <div className="container-button summary">
-                        <p>More Detail</p>
-                    </div>
-                </Link>
             </div>
         )
     }
