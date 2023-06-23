b92749a05f406b2dfb8fcc4999cfdfc3df33a7f4
Asus Laptop <minhnle311@gmail.com>
2023-06-23 10:26:09 -0700

    Fixed Data Rendering Issue

--- client/src/pages/ProductDetailPage.js
+++ client/src/pages/ProductDetailPage.js
@@ -5,53 +5,78 @@ import './_ProductDetailPage.css'
 import ProductNavbar from '../components/navbar/ProductNavbar'
 import Table from '../components/container/Table'
 import Spinner from '../components/spinner/Spinner'
+import ShowItemContainer from '../components/container/ShowItemContainer'
 
 const ProductDetailPage = () => {
     const {id} = useParams()
     const { isLoading, apiData, serverErr } = useFetch(`/api/v1/products/product/${id}`)
 
-    const product = apiData[0]
-
-    if (isLoading || product === undefined) return <Spinner />
+    if (isLoading) return <Spinner />
     else if (serverErr) return (<div>404</div>)
     else {
-    return (
-        <div className="page product-detail-page">
-            <ProductNavbar />
-            
-            <main>
-                <div className="product-detail info">
-                    <div className="product-detail-image-container">{product.image}</div>
-                    <div className='product-detail-info-container'>
-                        <div className="product-detail-name">{product.name}</div>
-                        <div className="product-detail-brand">{product.brand}</div>
-                        <div className="product-detail-sku">{product.sku}</div>
-                        <div className="product-detail-supplier">{product.supplier}</div>
-                    </div>
-                </div>
+        const total_sales = apiData.sales 
+                                ? apiData.sales.reduce((sum, curr) => {
+                                    return sum + curr.quantity
+                                    }, 0) 
+                                : <Spinner />
 
-                <div className="product-detail summary">
-                    <SummaryContainer />
-                    <SummaryContainer />
-                    <SummaryContainer />
-                </div>
+        const total_inv = apiData.inventory 
+                            ? apiData.inventory.reduce((sum, curr) => {
+                                if (curr.sum === undefined) curr.sum = 0
+                                return sum + Number(curr.sum)
+                                }, 0)
+                            : <Spinner />
 
-                <div className='product-detail detail'>
-                    <div className='product-detail-recent-sales'>
-                        <Table 
-                            header_array={['Name', 'Type', 'Brand', 'Supplier', 'SKU', 'Content', 'Images', 'Expire Date', 'Price', 'Discount']}
-                            data_array={[]}/>
+        return (
+            <div className="page product-detail-page">
+                <ProductNavbar />
+                
+                <main>
+                    <div className="product-detail info">
+                        <div className="product-detail-image-container">{apiData.general ? apiData.general.image : <Spinner />}</div>
+                        <div className='product-detail-info-container'>
+                            <div className="product-detail-name">{apiData.general ? apiData.general.product_name : <Spinner />}</div>
+                            <div className='product-detail-content'>{apiData.general ? apiData.general.content : <Spinner />}</div>
+                            <div className="product-detail-brand">{apiData.general ? apiData.general.brand : <Spinner />}</div>
+                            <div className="product-detail-sku">{apiData.general ? apiData.general.sku : <Spinner />}</div>
+                            <div className="product-detail-supplier">{apiData.general ? apiData.general.supplier_name : <Spinner />}</div>
+                        </div>
                     </div>
-                    <div className='product-detail-recent-purchase'>
-                        <Table 
-                            header_array={['Name', 'Type', 'Brand', 'Supplier', 'SKU', 'Content', 'Images', 'Expire Date', 'Price', 'Discount']}
-                            data_array={[]}
-                            />
+
+                    <div className="product-detail summary">
+                        <SummaryContainer data_to_show={total_sales} text_to_show='Total Sales'/>
+                        <SummaryContainer id='inventory' data_to_show={total_inv} text_to_show='Total Inventory'/>
+                        <ShowItemContainer 
+                            productName={apiData.general ? apiData.general.product_name : <Spinner />}
+                            productImg={apiData.general ? apiData.general.image : <Spinner />}
+                            showName={apiData.show ? apiData.show.show : 'N/A'}
+                            showDate={apiData.show ? apiData.show.date : 'N/A'}
+                            showPrice={apiData.show ? apiData.show.price : 'N/A'}
+                            showContent={apiData.show ? apiData.show.content : 'N/A'}    
+                        />
                     </div>
-                </div>
-            </main>
-        </div>
-    )
-}}
+
+                    <div className='product-detail detail'>
+                        <div className='product-detail-recent-sales'>
+                            <div className='title'>Recent Sales</div>
+                            <Table 
+                                header_array={['Code','Sale Date', 'Quantity', 'Customer', 'Payment Method','Store', 'Staff']}
+                                data_array={apiData.sales ? apiData.sales : []}
+                                mainData='sale'/>
+                        </div>
+                        <div className='product-detail-recent-purchase'>
+                            <div className='title'>Recent Purchases</div>
+                            <Table 
+                                header_array={['Code', 'Purchased Date', 'Quantity', 'Store', 'Supplier', 'Staff']}
+                                data_array={apiData.purchases ? apiData.purchases : []}
+                                mainData='purchase'
+                                />
+                        </div>
+                    </div>
+                </main>
+            </div>
+        )
+    }
+}
 
 export default ProductDetailPage
