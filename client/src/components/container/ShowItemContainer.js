ed119a5b39c2b92e3b920ab1ee57600db4ec1824
Asus Laptop <minhnle311@gmail.com>
2023-06-23 10:25:11 -0700

    Added Show Item Container

---
+++ client/src/components/container/ShowItemContainer.js
@@ -0,0 +1,23 @@
+import './_ShowItemContainer.css'
+
+const ShowItemContainer = ({
+        productName, 
+        productImg, 
+        showName, 
+        showDate, 
+        showPrice, 
+        showContent
+    }) => {
+    return (
+        <div className="show-item-container">
+            <div className="item-name">{productName}</div>
+            <div className="item-image">{productImg}</div>
+            <div className="show-name">{showName}</div>
+            <div className="show-date">{showDate}</div>
+            <div className="show-price">{showPrice}</div>
+            <div className="show-content">{showContent}</div>
+        </div>
+    )
+}
+
+export default ShowItemContainer
