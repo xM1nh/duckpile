6c26b997a82f69d13025a1d0beb2509527de7f6f
Asus Laptop <minhnle311@gmail.com>
2023-06-28 16:40:30 -0700

    Fixed queries for images

--- server/api/product/productQueries.ts
+++ server/api/product/productQueries.ts
@@ -5,17 +5,18 @@ export const get_all_products = `SELECT
                                     suppliers.name as supplier_name,
                                     products.sku,
                                     products.content,
-                                    products.images,
+                                    images.file_paths[1] as image,
                                     to_char(products.expired_date, 'MM-DD-YYYY') as expired_date,
                                     products.price,
                                     products.discount,
                                     products.id as product_id,
                                     suppliers.id as supplier_id
                                 FROM products
-                                    INNER JOIN suppliers ON products.supplier = suppliers.id`
+                                    INNER JOIN suppliers ON products.supplier = suppliers.id
+                                    LEFT JOIN images ON products.id = images.product_id`
 export const get_product_count = 'SELECT count(*) FROM products'
 export const get_paginated_products = `SELECT 
-                                        products.images,
+                                        images.file_paths[1] as image,
                                         products.name as product_name,
                                         products.type,
                                         products.brand,
@@ -28,7 +29,8 @@ export const get_paginated_products = `SELECT
                                         products.id as product_id,
                                         suppliers.id as supplier_id
                                     FROM products
-                                        INNER JOIN suppliers ON products.supplier = suppliers.id 
+                                        INNER JOIN suppliers ON products.supplier = suppliers.id
+                                        LEFT JOIN images ON products.id = images.product_id
                                     ORDER BY products.name
                                     LIMIT $1 OFFSET $2`
 export const sort_all_products = 'SELECT * FROM products ORDER BY $1 $2'
@@ -39,14 +41,15 @@ export const product_general_detail = `SELECT
                                             suppliers.name as supplier_name,
                                             products.sku,
                                             products.content,
-                                            products.images,
+                                            images.file_paths[0] as image,
                                             to_char(products.expired_date, 'MM-DD-YYYY') as expired_date,
                                             products.price,
                                             products.discount,
                                             products.id as product_id,
                                             suppliers.id as supplier_id
                                         FROM products
-                                            INNER JOIN suppliers ON products.supplier = suppliers.id 
+                                            INNER JOIN suppliers ON products.supplier = suppliers.id
+                                            LEFT JOIN images ON products.id = images.product_id 
                                         WHERE products.id=$1`
 export const product_sales_detail = `SELECT 
                                         sales.id as sale_name,
@@ -104,5 +107,25 @@ export const product_show_detail = `SELECT
                                         INNER JOIN products ON show.item = products.id
                                     WHERE show.item = $1
                                     ORDER BY show.date DESC`
-export const product_create = 'INSERT INTO products (name, type, brand, supplier, sku, content, images, expired_date, price, discount) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)'
+export const product_create = `INSERT INTO products (
+                                    name, 
+                                    type, 
+                                    brand, 
+                                    supplier, 
+                                    sku, 
+                                    content, 
+                                    expired_date, 
+                                    price, 
+                                    discount) 
+                                VALUES (
+                                    $1, 
+                                    $2, 
+                                    $3, 
+                                    $4, 
+                                    NULLIF($5, '')::int, 
+                                    $6, 
+                                    NULLIF($7, '')::date, 
+                                    $8, 
+                                    NULLIF($9, '')::decimal)
+                                RETURNING id;`
 export const product_update = ''
