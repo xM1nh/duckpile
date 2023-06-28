ca1ee682f856f70361018a88add2d46c42aaecf4
Asus Laptop <minhnle311@gmail.com>
2023-06-28 16:39:12 -0700

    Added route for image api, image rendering

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ const app = express()
 const port = 3001
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 
 import productRoute from './api/product/productRoutes'
 import purchaseRoute from './api/purchase/purchaseRoutes'
@@ -14,6 +15,7 @@ import inventoryRoute from './api/inventory/inventoryRoutes'
 import customerRoute from './api/customer/customerRoutes'
 import saleRoute from './api/sale/saleRoutes'
 import supplierRoute from './api/supplier/supplierRoutes'
+import imageRoute from './api/images/imageRoutes'
 
 app.use('/api/v1/products', productRoute)
 app.use('/api/v1/purchases', purchaseRoute)
@@ -21,6 +23,8 @@ app.use('/api/v1/inventory', inventoryRoute)
 app.use('/api/v1/customers', customerRoute)
 app.use('/api/v1/sales', saleRoute)
 app.use('/api/v1/suppliers', supplierRoute)
+app.use('/api/v1/uploads', imageRoute)
+app.use('/uploads', express.static(__dirname + '/uploads'))
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
