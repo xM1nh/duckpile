7b2b8e3ceb5d966a272c65cfc6671550698fbc22
Asus Laptop <minhnle311@gmail.com>
2023-06-28 16:40:50 -0700

    Fixed for images

--- server/api/product/productController.ts
+++ server/api/product/productController.ts
@@ -2,10 +2,20 @@ import { body, validationResult } from 'express-validator'
 import pool from '../../database'
 import * as product_queries from './productQueries'
 import asyncHandler from 'express-async-handler'
+import path from 'path'
 
 export const product_list = asyncHandler(async (req, res, next) => {
     const products = await pool.query(product_queries.get_all_products)
-    res.status(200).json(products.rows)
+    const productsUpdatedURL = products.rows.map((row: any) => {
+        if (row.image) {
+           const {image, ...rest} = row
+           const imagePath = path.join('..', image)
+           return {...rest, imagePath}
+        } else {
+            return row
+        }
+    })
+    res.status(200).json(productsUpdatedURL)
 })
 
 export const paginated_product_get = asyncHandler(async (req, res, next) => {
@@ -13,16 +23,32 @@ export const paginated_product_get = asyncHandler(async (req, res, next) => {
     const limit = parseInt(req.params.limit)
     const offset = (page - 1) * limit
     const products = await pool.query(product_queries.get_paginated_products, [limit, offset])
-    res.status(200).json(products.rows)
+    const productsUpdatedURL = products.rows.map((row: any) => {
+        if (row.image) {
+           const {image, ...rest} = row
+           const imagePath = path.join('..', image)
+           return {imagePath, ...rest}
+        } else {
+            return row
+        }
+    })
+    res.status(200).json(productsUpdatedURL)
 })
 
 export const product_detail = asyncHandler(async (req, res, next) => {
     const product_id = parseInt(req.params.id)
+
     const general = await pool.query(product_queries.product_general_detail, [product_id])
+    const {image, ...rest} = general.rows[0]
+    if (image) {
+        const imagePath = path.join('..', image)
+        general.rows[0].image = imagePath
+    }
     const sales = await pool.query(product_queries.product_sales_detail, [product_id])
     const purchase = await pool.query(product_queries.product_purchase_detail, [product_id])
     const inventory = await pool.query(product_queries.product_inventory_detail, [product_id])
     const show = await pool.query(product_queries.product_show_detail, [product_id])
+    
     res.status(200).json({general: general.rows[0], inventory: inventory.rows, sales: sales.rows, purchases: purchase.rows, show: show.rows[0]})
 })
 
@@ -66,13 +92,17 @@ export const product_create_post = [
             discount: req.body.discount,
             supplier: req.body.supplier,
             expDate: req.body.expDate,
-            content: req.body.content
+            content: req.body.content,
         }
 
         if(!errors.isEmpty()) {
             res.status(400).json({ errors })
         } else {
-            console.log(product)
+            //This will return the newly created product's id
+            const create_product = await pool.query(product_queries.product_create, [product.name, product.type, product.brand, product.supplier, product.sku, product.content, product.expDate, product.price, product.discount])
+            const productID = create_product.rows[0].id
+
+            res.status(200).json({message: 'Success', productID})
         }
     })
 ]
