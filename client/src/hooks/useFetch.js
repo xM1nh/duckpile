766064fdeabcc7df82b854b8a33f7c2e54c1653b
Asus Laptop <minhnle311@gmail.com>
2023-06-28 16:38:49 -0700

    Added method, body

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const useFetch = (url) => {
+const useFetch = (url, method, body) => {
     const [isLoading, setIsLoading] = useState(false)
     const [apiData, setApiData] = useState([])
     const [serverErr, setServerErr] = useState(null)
@@ -9,12 +9,13 @@ const useFetch = (url) => {
         const fetchData = async () => {
             setIsLoading(true)
             try {
-                const res = await fetch(url)
+                const res = await fetch(url, {
+                    method: method,
+                    body: body
+                })
                 if (!res.ok) throw new Error(res.statusText)
-
                 const data = await res.json()
                 setApiData(data)
-                
                 setIsLoading(false)
                 setServerErr(null)
             } catch (err) {
