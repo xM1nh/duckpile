db5ad76123880ef6c1cb12a311b022fbc6c72a69
Asus Laptop <minhnle311@gmail.com>
2023-07-02 17:35:50 -0700

    Added default value

--- client/src/components/forms/FormInput.js
+++ client/src/components/forms/FormInput.js
@@ -1,14 +1,17 @@
 import './_FormInput.css'
 
-const FormInput = ({id, label, input, inputType, placeholder, err, handleChange}) => {
+const FormInput = ({id, label, input, inputType, placeholder, err, handleChange, defaultValue}) => {
     return (
         <div className="FormInput" id={id}>
-            <label htmlFor={input}>{label}</label>
+            <div className='label'>
+                <label htmlFor={input}>{label}</label>
+            </div>
             <input 
                 type={inputType ? inputType : 'text'} 
                 name={input} 
                 placeholder={placeholder}
-                onChange={handleChange}>
+                onChange={handleChange}
+                defaultValue={defaultValue}>
             </input>
             {err && <p>*{err.message}</p>}
         </div>
