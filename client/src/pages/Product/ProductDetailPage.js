1c1bdf93e90a21ab6d65773a0747c23d58220388
Asus Laptop <minhnle311@gmail.com>
2023-07-02 17:38:04 -0700

    Refactored

--- client/src/pages/Product/ProductDetailPage.js
+++ client/src/pages/Product/ProductDetailPage.js
@@ -7,32 +7,47 @@ import Spinner from '../../components/spinner/Spinner'
 import ShowItemContainer from '../../components/container/ShowItemContainer'
 import ButtonContainer from '../../components/buttons/ButtonContainer'
 import MainNavbar from '../../components/navbar/MainNavbar'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
 const ProductDetailPage = () => {
     const {id} = useParams()
     const [mainImg, setMainImg] = useState(null)
+    const [data, setData] = useState({})
+    const [sale, setSale] = useState([])
+    const [purchase, setPurchase] = useState([])
+    const [images, setImages] = useState([])
     const { isLoading, apiData, serverErr } = useFetch(`/api/v1/products/product/${id}`)
 
     const handleImageClick = (e) => {
         setMainImg(e.target.src)
     }
 
+    const total_sales = sale.reduce((sum, curr) => {
+        return sum + curr.quantity
+        }, 0) 
+
+    const total_inv = parseInt(data.inventory_store_1) + parseInt(data.inventory_store_2) + parseInt(data.inventory_store_3)
+
+    useEffect(() => {
+        if (apiData.general) {
+            setData(apiData.general)
+        }
+        if (apiData.sales) {
+            setSale(apiData.sales)
+        }
+        if (apiData.purchases) {
+            setPurchase(apiData.purchases)
+        }
+        if (apiData.images) {
+            setImages(apiData.images)
+            setMainImg(apiData.images[0])
+        }
+    }, [apiData.general, apiData.sales, apiData.purchases, apiData.images])
+
     if (isLoading) return <Spinner />
     else if (serverErr) return (<div>404</div>)
     else {
-        const total_sales = apiData.sales 
-                                ? apiData.sales.reduce((sum, curr) => {
-                                    return sum + curr.quantity
-                                    }, 0) 
-                                : <Spinner />
-
-        const total_inv = apiData.inventory 
-                            ? apiData.inventory.reduce((sum, curr) => {
-                                if (curr.sum === undefined) curr.sum = 0
-                                return sum + Number(curr.sum)
-                                }, 0)
-                            : <Spinner />
         return (
             <div className="page product-detail-page">
                 <MainNavbar />
@@ -41,30 +56,37 @@ const ProductDetailPage = () => {
                     <div className="product-detail info">
                         <div className="product-detail-image-container">
                             <div className='image-list'>
-                                {apiData.images && 
-                                    apiData.images.map(image => {
+                                {images.map(image => {
                                         return (
-                                            <img onClick={handleImageClick} src={image} alt=''></img>
+                                            <img key={uuidv4()} onClick={handleImageClick} src={image} alt=''></img>
                                         )
-                                })}
+                                    }
+                                )}
                             </div>
                             <div className='main-image'><img src={mainImg} alt=''></img></div>
                         </div>
                         <div className='product-detail-info-container'>
-                            <div className="product-detail-name">{apiData.general ? apiData.general.product_name : <Spinner />}</div>
-                            <div className='product-detail-content'>{apiData.general ? apiData.general.content : <Spinner />}</div>
-                            <div className="product-detail-brand">{apiData.general ? apiData.general.brand : <Spinner />}</div>
-                            <div className="product-detail-sku">{apiData.general ? apiData.general.sku : <Spinner />}</div>
-                            <div className="product-detail-supplier">{apiData.general ? apiData.general.supplier_name : <Spinner />}</div>
+                            <div className="product-detail-name">{data.product_name}</div>
+                            <div className='price'>
+                                <div className='product-detail-price'>Regular Price: {data.price}</div>
+                                <div className='product-detail-discount'>Discount: {data.discount}</div>
+                            </div>
+                            <div className='product-detail-type'>Type: {data.type}</div>
+                            <div className='product-detail-content'>{data.content}</div>
+                            <div className='supplier'>
+                                <div className="product-detail-brand">Brand: {data.brand}</div>
+                                <div className="product-detail-sku">SKU: {data.sku}</div>
+                                <div className="product-detail-supplier">Supplier: {data.supplier_name}</div>
+                            </div>
                         </div>
                     </div>
 
                     <div className="product-detail summary">
                         <SummaryContainer data_to_show={total_sales} text_to_show='Total Sales'/>
-                        <SummaryContainer id='inventory' data_to_show={total_inv} text_to_show='Total Inventory'/>
+                        <SummaryContainer data_to_show={total_inv} text_to_show='Total Inventory'/>
                         <ShowItemContainer 
-                            productName={apiData.general ? apiData.general.product_name : <Spinner />}
-                            productImg={apiData.general ? apiData.general.image : <Spinner />}
+                            productName={data.product_name}
+                            productImg={images[0]}
                             showName={apiData.show ? apiData.show.show : 'N/A'}
                             showDate={apiData.show ? apiData.show.date : 'N/A'}
                             showPrice={apiData.show ? apiData.show.price : 'N/A'}
@@ -77,14 +99,14 @@ const ProductDetailPage = () => {
                             <div className='title'>Recent Sales</div>
                             <Table 
                                 header_array={['Code','Sale Date', 'Quantity', 'Customer', 'Payment Method','Store', 'Staff']}
-                                data_array={apiData.sales ? apiData.sales : []}
+                                data_array={sale}
                                 mainData='sale'/>
                         </div>
                         <div className='product-detail-recent-purchase'>
                             <div className='title'>Recent Purchases</div>
                             <Table 
                                 header_array={['Code', 'Purchased Date', 'Quantity', 'Store', 'Supplier', 'Staff']}
-                                data_array={apiData.purchases ? apiData.purchases : []}
+                                data_array={purchase}
                                 mainData='purchase'
                                 />
                         </div>
