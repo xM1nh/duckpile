020da7916864054e78a6837dee9c3197090909cf
Asus Laptop <minhnle311@gmail.com>
2023-07-02 17:34:42 -0700

    Added edit route

--- client/src/RouteSwitch.js
+++ client/src/RouteSwitch.js
@@ -8,7 +8,8 @@ import SalesPage from './pages/Sale/SalesPage'
 import InventoryPage from "./pages/Inventory/InventoryPage";
 import CustomerPage from "./pages/Customer/CustomerPage";
 import SupplierPage from "./pages/Supplier/SupplierPage";
-import CreateProductForm from "./pages/Product/CreateProductForm";
+import CreateProductForm from "./pages/Product/CreateProductPage";
+import EditProductPage from "./pages/Product/EditProductPage";
 
 
 const RouteSwitch = () => {
@@ -19,6 +20,7 @@ const RouteSwitch = () => {
                 <Route path='/products' element={<ProductListPage />} />
                 <Route path='/product/:id' element={<ProductDetailPage />} />
                 <Route path='/product/create' element={<CreateProductForm />}/>
+                <Route path='/product/:id/edit' element={<EditProductPage />} />
                 <Route path='/purchases' element={<PurchasePage />} />
                 <Route path='/sales' element={<SalesPage />} />
                 <Route path='/inventory' element={<InventoryPage />} />
