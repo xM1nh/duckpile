755dbc42c68ece0b83fd05c11de62b1683b7a283
Asus Laptop <minhnle311@gmail.com>
2023-07-02 17:34:23 -0700

    Fixed inventory query

--- server/api/inventory/inventoryController.ts
+++ server/api/inventory/inventoryController.ts
@@ -4,7 +4,20 @@ import asyncHandler from 'express-async-handler'
 
 export const inventory_list = asyncHandler(async (req, res, next) => {
     const inventory = await pool.query(inventory_queries.get_all_inventory)
-    res.status(200).json(inventory.rows)
+    const store1 = inventory.rows.map((row :{product_name: string, inventory_store_1: number, product_id: number}) => {
+        const {product_name, inventory_store_1, product_id, ...rest} = row
+        return {product_name, quantity: inventory_store_1, product_id}
+    })
+    const store2 = inventory.rows.map((row :{product_name: string, inventory_store_2: number, product_id: number}) => {
+        const {product_name, inventory_store_2, product_id, ...rest} = row
+        return {product_name, quantity: inventory_store_2, product_id}
+    })
+    const store3 = inventory.rows.map((row :{product_name: string, inventory_store_3: number, product_id: number}) => {
+        const {product_name, inventory_store_3, product_id, ...rest} = row
+        return {product_name, quantity: inventory_store_3, product_id}
+    })
+
+    res.status(200).json({store1, store2, store3})
 })
 
 export const inventory_detail = asyncHandler(async (req, res, next) => {
--- server/api/inventory/inventoryQueries.ts
+++ server/api/inventory/inventoryQueries.ts
@@ -1,13 +1,10 @@
 export const get_all_inventory = `SELECT 
-                                    products.id as product_id, 
-                                    stores.store_name as store_name, 
-                                    products.name as product_name, 
-                                    SUM (inventory.quantity),
-                                    stores.id as store_id  
-                                FROM inventory 
-                                    JOIN products ON inventory.item = products.id 
-                                    JOIN stores ON inventory.store = stores.id
-                                GROUP BY products.id, stores.store_name, stores.id
+                                    products.name as product_name,
+                                    coalesce(products.inventory_store_1, 0) as inventory_store_1,
+                                    coalesce(products.inventory_store_2, 0) as inventory_store_2,
+                                    coalesce(products.inventory_store_3, 0) as inventory_store_3,
+                                    products.id as product_id 
+                                FROM products 
                                 ORDER BY products.id DESC`
 export const sort = 'SELECT * FROM inventory ORDER BY $1 $2'
 export const inventory_detail = 'SELECT * FROM inventory WHERE id = $1'
