3f5fe3b50954beb2d6f1b1c315e456a192008837
Asus Laptop <minhnle311@gmail.com>
2023-07-02 17:26:18 -0700

    Removed delete GET route

--- server/api/product/productRoutes.ts
+++ server/api/product/productRoutes.ts
@@ -10,11 +10,9 @@ router.get('/product/:id', product_controller.product_detail)
 
 router.post('/product/create', product_controller.product_create_post)
 
-router.get('/product/:id/delete', product_controller.product_delete_get)
-
 router.post('/product/:id/delete', product_controller.product_delete_post)
 
-router.get('/product/:id/udpate', product_controller.product_update_get)
+router.get('/product/:id/update', product_controller.product_update_get)
 
 router.post('/product/:id/update', product_controller.product_update_post)
 
