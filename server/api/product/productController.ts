55aaafc85d1a01e4433949fdb08246a193a18b15
Asus Laptop <minhnle311@gmail.com>
2023-07-02 17:34:03 -0700

    Fixed inventory controller. Added update POST GET

--- server/api/product/productController.ts
+++ server/api/product/productController.ts
@@ -40,7 +40,10 @@ export const product_detail = asyncHandler(async (req, res, next) => {
 
     const general = await pool.query(product_queries.product_general_detail, [product_id])
     const images = await pool.query(product_queries.product_images, [product_id])
-    
+    const sales = await pool.query(product_queries.product_sales_detail, [product_id])
+    const purchase = await pool.query(product_queries.product_purchase_detail, [product_id])
+    const show = await pool.query(product_queries.product_show_detail, [product_id])
+
     const imgUrl = images.rows[0]
                     ? images.rows[0].file_paths.map((file_path : any) => {
                         const url = path.join('..', file_path)
@@ -48,13 +51,8 @@ export const product_detail = asyncHandler(async (req, res, next) => {
                         return url
                     })
                     : []
-
-    const sales = await pool.query(product_queries.product_sales_detail, [product_id])
-    const purchase = await pool.query(product_queries.product_purchase_detail, [product_id])
-    const inventory = await pool.query(product_queries.product_inventory_detail, [product_id])
-    const show = await pool.query(product_queries.product_show_detail, [product_id])
     
-    res.status(200).json({general: general.rows[0], images: imgUrl, inventory: inventory.rows, sales: sales.rows, purchases: purchase.rows, show: show.rows[0]})
+    res.status(200).json({general: general.rows[0], images: imgUrl, inventory: '', sales: sales.rows, purchases: purchase.rows, show: show.rows[0]})
 })
 
 export const product_create_post = [
@@ -84,6 +82,15 @@ export const product_create_post = [
     body('content')
         .trim()
         .escape(),
+    body('store1_inv')
+        .trim()
+        .escape(),
+    body('store2_inv')
+        .trim()
+        .escape(),
+    body('store3_inv')
+        .trim()
+        .escape(),
         
     asyncHandler(async (req, res, next) => {
         const errors = validationResult(req)
@@ -96,8 +103,11 @@ export const product_create_post = [
             price: req.body.price,
             discount: req.body.discount,
             supplier: req.body.supplier,
-            expDate: req.body.expDate,
+            expDate: req.body.expired_date,
             content: req.body.content,
+            store1_inv: req.body.store1_inv,
+            store2_inv: req.body.store2_inv,
+            store3_inv: req.body.store3_inv,
         }
 
         if(!errors.isEmpty()) {
@@ -112,18 +122,89 @@ export const product_create_post = [
     })
 ]
 
-export const product_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: product delete get')
-})
-
 export const product_delete_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: product delete post')
+    const productID = parseInt(req.params.id)
+    await pool.query(product_queries.product_delete, [productID])
+    res.status(200).json({message: 'Success'})
 })
 
 export const product_update_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: product update get')
+    const product_id = parseInt(req.params.id)
+    const general = await pool.query(product_queries.product_general_detail, [product_id])
+    const images = await pool.query(product_queries.product_images, [product_id])
+    
+    const imgUrl = images.rows[0]
+                    ? images.rows[0].file_paths.map((file_path : any) => {
+                        const url = path.join('..', file_path)
+                        file_path = url
+                        return url
+                    })
+                    : []
+    res.status(200).json({general: general.rows[0], images: imgUrl})
 })
 
-export const product_update_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: product update post')
-})
+export const product_update_post = [
+    body('product_name', 'Product name must not be empty.')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('type')
+        .trim()
+        .escape(),
+    body('brand')
+        .trim()
+        .escape(),
+    body('sku')
+        .trim()
+        .escape(),
+    body('price', 'Product price must not be empty.')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('discount')
+        .trim()
+        .escape(),
+    body('expired_date')
+        .trim()
+        .escape(),
+    body('content')
+        .trim()
+        .escape(),
+    body('store1_inv')
+        .trim()
+        .escape(),
+    body('store2_inv')
+        .trim()
+        .escape(),
+    body('store3_inv')
+        .trim()
+        .escape(),
+        
+    asyncHandler(async (req, res, next) => {
+        const id = parseInt(req.params.id)
+        const errors = validationResult(req)
+        const product = {
+            name: req.body.product_name,
+            type: req.body.type,
+            brand: req.body.brand,
+            sku: req.body.sku,
+            price: Number(req.body.price),
+            discount: Number(req.body.discount),
+            supplier: parseInt(req.body.supplier_id),
+            expDate: req.body.expired_date,
+            content: req.body.content,
+            store1_inv: parseInt(req.body.inventory_store_1),
+            store2_inv: parseInt(req.body.inventory_store_2),
+            store3_inv: parseInt(req.body.inventory_store_3),
+        }
+        
+        if(!errors.isEmpty()) {
+            res.status(400).json({ errors })
+        } else {
+            //This will return the newly created product's id
+            const create_product = await pool.query(product_queries.product_update, [product.name, product.type, product.brand, product.supplier, product.sku, product.content, product.expDate, product.price, product.discount, product.store1_inv, product.store2_inv, product.store3_inv, id])
+
+            res.status(200).json({message: 'Success'})
+        }
+    })
+]
