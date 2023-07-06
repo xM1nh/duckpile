4acd6ee11d7ab241de3516c8a0c2bdf16da7f70a
Asus Laptop <minhnle311@gmail.com>
2023-07-06 10:34:57 -0700

    Implemented create, edit, detail controller

--- server/api/customer/customerController.ts
+++ server/api/customer/customerController.ts
@@ -1,6 +1,7 @@
 import pool from '../../database'
 import * as customer_queries from './customerQueries'
 import asyncHandler from 'express-async-handler'
+import { body, validationResult } from 'express-validator'
 
 export const customer_list = asyncHandler(async (req, res, next) => {
     const customers = await pool.query(customer_queries.get_all_customers)
@@ -8,29 +9,137 @@ export const customer_list = asyncHandler(async (req, res, next) => {
 })
 
 export const customer_detail = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: customer detail')
+    const id = parseInt(req.params.id)
+    const customer = await pool.query(customer_queries.customer_detail, [id])
+    const sales = await pool.query(customer_queries.customer_buy_detail, [id])
+
+    const totalSales = sales.rows.length
+    const totalValue = sales.rows.reduce((sum: number, curr: {quantity: number, xxxidxxx: number}) => {
+        return sum + curr.quantity * curr.xxxidxxx
+    }, 0)
+
+    const mostBuyedProduct = () => {
+        const productMap: {[key:string]: number} = {}
+        sales.rows.forEach((row: {product_name: string}) => {
+            if (!productMap[row.product_name]) {
+                productMap[row.product_name] = 1
+            } else {
+                productMap[row.product_name]++
+            }
+        })
+
+        const returns = Object.entries(productMap).sort((a,b) => b[1] - a[1])
+        return returns.shift()
+    }
+
+    res.status(200).json({general: customer.rows[0], sales: {list: sales.rows, mostBuyedProduct: mostBuyedProduct(), totalSales: totalSales, totalValue: totalValue}})
 })
 
-export const customer_create_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: customer create get')
-})
+export const customer_create_post = [
+    body('firstName', 'First name must not be empty')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('lastName', 'Last name must not be empty')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('phoneNumber')
+        .trim()
+        .isLength({min: 10, max: 10})
+        .escape(),
+    body('street')
+        .trim()
+        .escape(),
+    body('city')
+        .trim()
+        .escape(),
+    body('state')
+        .trim()
+        .escape(),
+    body('zip')
+        .trim()
+        .escape(),
 
-export const customer_create_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: customer create post')
-})
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req)
 
-export const customer_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: customer delete get')
-})
+        const customer = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: parseInt(req.body.phoneNumber),
+            street: req.body.street === '' ? null : req.body.street,
+            city: req.body.city === '' ? null : req.body.city,
+            state: req.body.state === '' ? null : req.body.state,
+            zip: req.body.zip === '' ? null : req.body.zip,
+        }
+
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors)
+        } else {
+            const createCustomer = await pool.query(customer_queries.customer_create, [customer.firstName, customer.lastName, customer.phoneNumber, customer.street, customer.city, customer.state, customer.zip])
+            const customerID = createCustomer.rows[0].id
+
+            res.status(200).json({message: 'Success', id: customerID})
+        }
+    })
+]
 
 export const customer_delete_post = asyncHandler(async (req, res, next) => {
     res.send('not implemented: customer delete post')
 })
 
 export const customer_update_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: customer update get')
+    const id = parseInt(req.params.id)
+    const customer = await pool.query(customer_queries.customer_detail, [id])
+    res.status(200).json(customer.rows[0])
 })
 
-export const customer_update_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: customer update post')
-})
+export const customer_update_post = [
+    body('first_name', 'First name must not be empty')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('last_name', 'Last name must not be empty')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('phone_number')
+        .trim()
+        .isLength({min: 10, max: 10})
+        .escape(),
+    body('street')
+        .trim()
+        .escape(),
+    body('city')
+        .trim()
+        .escape(),
+    body('state')
+        .trim()
+        .escape(),
+    body('zip')
+        .trim()
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const id = parseInt(req.params.id)
+        const errors = validationResult(req)
+
+        const customer = {
+            firstName: req.body.first_name,
+            lastName: req.body.last_name,
+            phoneNumber: parseInt(req.body.phone_number),
+            street: req.body.street === '' ? null : req.body.street,
+            city: req.body.city === '' ? null : req.body.city,
+            state: req.body.state === '' ? null : req.body.state,
+            zip: req.body.zip === '' ? null : req.body.zip,
+        }
+
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors)
+        } else {
+            await pool.query(customer_queries.customer_update, [customer.firstName, customer.lastName, customer.street, customer.city, customer.state, customer.zip, customer.phoneNumber, id])
+            res.status(200).json({message: 'Updated'})
+        }
+    })
+]
--- server/api/customer/customerQueries.ts
+++ server/api/customer/customerQueries.ts
@@ -1,9 +1,52 @@
-export const get_all_customers = `SELECT first_name || ' ' || last_name as customer_name,
-                                    street || ', ' || city || ', ' || state as address,
+export const get_all_customers = `SELECT 
+                                    concat(first_name, ' ', last_name) as customer_name,
+                                    concat(street, coalesce(', ' || city, ''), coalesce(', ' || state, ''), coalesce(' ' || zip, '')) as address,
                                     phone_number, 
                                     id as customer_id
                                     FROM customers`
 export const sort = 'SELECT * FROM customers ORDER BY $1 $2'
-export const customer_detail = 'SELECT * FROM customers WHERE id = $1'
-export const customer_create = 'INSERT INTO customers (first_name, last_name, street, city, state, phone_number) values ($1, $2, $3, $4, $5, $6)'
-export const customer_update = ''
+export const customer_detail = `SELECT 
+                                    first_name,
+                                    last_name,
+                                    coalesce(street, '') as street,
+                                    coalesce(city, '') as city,
+                                    coalesce(state, '') as state,
+                                    coalesce(zip::text, '') as zip,
+                                    phone_number, 
+                                    id as customer_id
+                                FROM customers 
+                                WHERE id = $1`
+export const customer_buy_detail = `SELECT
+                                        sales.id as sale_name,
+                                        products.name as product_name,
+                                        products.price as xxxidxxx,
+                                        sales.quantity,
+                                        to_char(sales.sale_date, 'MM-DD-YYYY') as sale_date,
+                                        stores.store_name as store_name,
+                                        staffs.first_name || ' ' || staffs.last_name as staff_name,
+                                        products.id as product_id,
+                                        sales.id as sale_id
+                                    FROM sales
+                                        INNER JOIN products ON sales.item = products.id
+                                        INNER JOIN stores ON sales.store = stores.id
+                                        INNER JOIN staffs ON sales.staff = staffs.id
+                                    WHERE sales.customer = $1`
+export const customer_create = `INSERT INTO customers (
+                                    first_name, 
+                                    last_name,
+                                    phone_number, 
+                                    street, 
+                                    city, 
+                                    state, 
+                                    zip) 
+                                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                                RETURNING id`
+export const customer_update = `UPDATE customers
+                                SET first_name = $1,
+                                    last_name = $2,
+                                    street = $3,
+                                    city = $4,
+                                    state = $5,
+                                    zip = $6,
+                                    phone_number = $7
+                                WHERE id = $8`
