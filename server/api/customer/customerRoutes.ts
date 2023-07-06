794e26f8b2c0ec7da74f633924dadcd73a35dbb3
Asus Laptop <minhnle311@gmail.com>
2023-07-06 10:35:49 -0700

    Removed unnecessary routes

--- server/api/customer/customerRoutes.ts
+++ server/api/customer/customerRoutes.ts
@@ -6,15 +6,11 @@ router.get('/', customer_controller.customer_list)
 
 router.get('/customer/:id', customer_controller.customer_detail)
 
-router.get('/customer/create', customer_controller.customer_delete_get)
-
 router.post('/customer/create', customer_controller.customer_create_post)
-
-router.get('/customer/:id/delete', customer_controller.customer_delete_get)
 
 router.post('/customer/:id/delete', customer_controller.customer_delete_post)
 
-router.get('/customer/:id/udpate', customer_controller.customer_update_get)
+router.get('/customer/:id/update', customer_controller.customer_update_get)
 
 router.post('/customer/:id/update', customer_controller.customer_update_post)
 
