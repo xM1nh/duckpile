20d77b8aca5864d4331ae2f818a451378f8106ae
Asus Laptop <minhnle311@gmail.com>
2023-07-11 13:05:32 -0700

    Edited Routing

--- server/api/product/productRoutes.ts
+++ server/api/product/productRoutes.ts
@@ -4,11 +4,9 @@ const router = express.Router()
 
 router.get('/', product_controller.product_list)
 
-router.get('/pages/:page/:limit', product_controller.paginated_product_get)
-
 router.get('/product/:id', product_controller.product_detail)
 
-router.post('/product/create', product_controller.product_create_post)
+router.post('/create', product_controller.product_create_post)
 
 router.post('/product/:id/delete', product_controller.product_delete_post)
 
