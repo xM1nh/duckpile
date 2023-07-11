5f903cb0b9501fdd82ec7f41014477af8fa108f0
Asus Laptop <minhnle311@gmail.com>
2023-07-11 13:09:45 -0700

    Refactored

--- client/src/pages/Product/ProductListPage.js
+++ client/src/pages/Product/ProductListPage.js
@@ -7,17 +7,21 @@ import Table from "../../components/container/Table"
 import SummaryContainer from "../../components/container/SummaryContainer"
 import ButtonContainer from "../../components/buttons/ButtonContainer"
 import DeleteModal from "../../components/modal/DeleteModal"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const itemNumPerPage = 10
-const itemCount = 9
 
 const ProductListPage = () => { 
-    const {currentPage, pageCount, handleNext, handlePrev, handlePage} = usePagination(itemCount, itemNumPerPage)
-    const url = `/api/v1/products/pages/${currentPage}/${itemNumPerPage}`
-    const {isLoading, apiData, serverErr} = useFetch(url)
+    const [data, setData] = useState({
+        list: [],
+        count: 0
+    })
     const [openModal, setOpenModal] = useState(false)
     const [deleteItem, setDeleteItem] = useState(null)
+    
+    const {currentPage, pageCount, handleNext, handlePrev, handlePage} = usePagination(data.count, itemNumPerPage)
+    const url = `/api/v1/products?page=${currentPage}&count=${itemNumPerPage}`
+    const {isLoading, apiData, serverErr} = useFetch(url)
 
     const tableOpenModal = (e) => {
         setOpenModal(true)
@@ -44,13 +48,22 @@ const ProductListPage = () => {
         })
     }
 
+    useEffect(() => {
+        if (apiData) {
+            setData({
+                list: apiData.products,
+                count: apiData.count
+            })
+        }
+    }, [apiData])
+
     return (
         <div className='page product_list'>
             {openModal && <DeleteModal handleCancelClick={closeModal} handleDeleteClick={handleDelete}/>}
             <MainNavbar />
             <main>
                 <section className="summary">
-                    <SummaryContainer data_to_show={apiData.length} text_to_show='Total Products'/>
+                    <SummaryContainer data_to_show={data.count} text_to_show='Total Products'/>
                     <SummaryContainer />
                     <SummaryContainer />
                 </section>
@@ -61,7 +74,7 @@ const ProductListPage = () => {
                             <Table
                                 mainData='product' 
                                 header_array={['Image', 'Name', 'Type', 'Brand', 'Supplier', 'SKU', 'Content', 'Expire Date', 'Price', 'Discount', 'Store 1 Inventory', 'Store 2 Inventory', 'Store 3 Inventory']}
-                                data_array={apiData}
+                                data_array={data.list}
                                 handleDelete={tableOpenModal}
                             />
                         </div>
