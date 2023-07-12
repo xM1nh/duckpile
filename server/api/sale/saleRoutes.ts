8981d29480802ed1efdbd80578bf21e1d96f905c
Asus Laptop <minhnle311@gmail.com>
2023-07-12 12:16:05 -0700

    Spelling

--- server/api/sale/saleRoutes.ts
+++ server/api/sale/saleRoutes.ts
@@ -12,7 +12,7 @@ router.post('/create', sale_controller.sale_create_post)
 
 router.post('/sale/:id/delete', sale_controller.sale_delete_post)
 
-router.get('/sale/:id/udpate', sale_controller.sale_update_get)
+router.get('/sale/:id/update', sale_controller.sale_update_get)
 
 router.post('/sale/:id/update', sale_controller.sale_update_post)
 
