e000ec09c0f5496e6954ec92ab019666a7d488c7
Asus Laptop <minhnle311@gmail.com>
2023-07-13 12:44:33 -0700

    Fixed Routing

--- server/api/supplier/supplierRoutes.ts
+++ server/api/supplier/supplierRoutes.ts
@@ -6,15 +6,11 @@ router.get('/', supplier_controller.supplier_list)
 
 router.get('/supplier/:id', supplier_controller.supplier_detail)
 
-router.get('/supplier/create', supplier_controller.supplier_delete_get)
-
-router.post('/supplier/create', supplier_controller.supplier_create_post)
-
-router.get('/supplier/:id/delete', supplier_controller.supplier_delete_get)
+router.post('/create', supplier_controller.supplier_create_post)
 
 router.post('/supplier/:id/delete', supplier_controller.supplier_delete_post)
 
-router.get('/supplier/:id/udpate', supplier_controller.supplier_update_get)
+router.get('/supplier/:id/update', supplier_controller.supplier_update_get)
 
 router.post('/supplier/:id/update', supplier_controller.supplier_update_post)
 
