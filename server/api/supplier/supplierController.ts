cdd9f6f7607258d4c5cba11fbc9fbe5110d47120
Asus Laptop <minhnle311@gmail.com>
2023-07-13 12:44:52 -0700

    Implemented all routes

--- server/api/supplier/supplierController.ts
+++ server/api/supplier/supplierController.ts
@@ -1,36 +1,152 @@
 import pool from '../../database'
 import * as supplier_queries from './supplierQueries'
 import asyncHandler from 'express-async-handler'
+import { body, validationResult } from 'express-validator'
 
 export const supplier_list = asyncHandler(async (req, res, next) => {
-    const suppliers = await pool.query(supplier_queries.get_all_suppliers)
-    res.status(200).json(suppliers.rows)
+    const pagination = {
+        limit: Number(req.query.count),
+        offset: (Number(req.query.page) - 1) * Number(req.query.count)
+    }
+    const suppliers = await pool.query(supplier_queries.get_all_suppliers, [pagination.limit, pagination.offset])
+    const suppliersCount = await pool.query(supplier_queries.get_suppliers_count)
+    res.status(200).json({suppliers: suppliers.rows, count: suppliersCount.rows[0].count})
 })
 
 export const supplier_detail = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier detail')
+    const id = parseInt(req.params.id)
+    const supplier = await pool.query(supplier_queries.supplier_detail, [id])
+    const sales = await pool.query(supplier_queries.supplier_purchase_detail, [id])
+    const mostPurchasedProduct = await pool.query(supplier_queries.supplier_most_purchase, [id])
+    const totalSales = sales.rows.length
+    const totalValue = sales.rows.reduce((sum: number, curr: {total_amount: number}) => {
+        return sum + curr.total_amount
+    }, 0)
+
+    const response = {
+        general: supplier.rows[0],
+        sales: {
+            list: sales.rows.map((row: {products: string[]}) => {
+                var {products, ...rest} = row
+                let result: string = ''
+                row.products.forEach(product => {
+                    const name = product.split(',')[0]
+                    const quantity = product.split(',')[1]
+                    const value = quantity + 'x' + ' ' + name + ', '
+                    result += value
+                })
+                return {result, ...rest}
+            }),
+            totalSales: totalSales,
+            totalValue: totalValue,
+            mostPurchasedProduct: mostPurchasedProduct.rows[0]
+        }
+    }
+
+    res.status(200).json(response)
 })
 
-export const supplier_create_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier create get')
-})
+export const supplier_create_post = [
+    body('name', 'Name must not be empty')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('phoneNumber')
+        .trim()
+        .isLength({min: 10, max: 10})
+        .escape()
+        .toInt(),
+    body('street')
+        .trim()
+        .escape(),
+    body('city')
+        .trim()
+        .escape(),
+    body('state')
+        .trim()
+        .escape(),
+    body('zip')
+        .trim()
+        .escape()
+        .toInt(),
 
-export const supplier_create_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier create post')
-})
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req)
 
-export const supplier_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier delete get')
-})
+        const supplier = {
+            name: req.body.name,
+            phoneNumber: req.body.phoneNumber,
+            street: req.body.street === '' ? null : req.body.street,
+            city: req.body.city === '' ? null : req.body.city,
+            state: req.body.state === '' ? null : req.body.state,
+            zip: req.body.zip === '' ? null : req.body.zip,
+        }
+
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors)
+        } else {
+            const createCustomer = await pool.query(supplier_queries.supplier_create, [supplier.name, supplier.phoneNumber, supplier.street, supplier.city, supplier.state, supplier.zip])
+            const supplierID = createCustomer.rows[0].id
+
+            res.status(200).json({message: 'Success', id: supplierID})
+        }
+    })
+]
 
 export const supplier_delete_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier delete post')
+    const id = parseInt(req.params.id)
+    await pool.query(supplier_queries.supplier_delete, [id])
+    res.status(200).json({message: 'Deleted'})
 })
 
 export const supplier_update_get = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier update get')
+    const id = parseInt(req.params.id)
+    const supplier = await pool.query(supplier_queries.supplier_detail, [id])
+    res.status(200).json(supplier.rows[0])
 })
 
-export const supplier_update_post = asyncHandler(async (req, res, next) => {
-    res.send('not implemented: supplier update post')
-})
+export const supplier_update_post = [
+    body('name', 'Name must not be empty')
+        .trim()
+        .isLength({min: 1})
+        .escape(),
+    body('phone_number')
+        .trim()
+        .isLength({min: 10, max: 10})
+        .escape()
+        .toInt(),
+    body('street')
+        .trim()
+        .escape(),
+    body('city')
+        .trim()
+        .escape(),
+    body('state')
+        .trim()
+        .escape(),
+    body('zip')
+        .trim()
+        .escape()
+        .toInt(),
+
+    asyncHandler(async (req, res, next) => {
+        const id = parseInt(req.params.id)
+        const errors = validationResult(req)
+
+        const supplier = {
+            name: req.body.name,
+            phoneNumber: req.body.phone_number,
+            street: req.body.street === '' ? null : req.body.street,
+            city: req.body.city === '' ? null : req.body.city,
+            state: req.body.state === '' ? null : req.body.state,
+            zip: req.body.zip === '' ? null : req.body.zip,
+        }
+
+        if (!errors.isEmpty()) {
+            res.status(400).json(errors)
+        } else {
+            await pool.query(supplier_queries.supplier_update, [supplier.name, supplier.street, supplier.city, supplier.state, supplier.zip, supplier.phoneNumber, id])
+            res.status(200).json({message: 'Updated'})
+        }
+    })
+]
--- server/api/supplier/supplierQueries.ts
+++ server/api/supplier/supplierQueries.ts
@@ -1,10 +1,58 @@
 export const get_all_suppliers = `SELECT 
                                     name as supplier_name,
-                                    street || ', ' || city || ', ' || state as address,
+                                    street || ', ' || city || ', ' || state || ' ' || zip as address,
                                     phone_number,
                                     suppliers.id as supplier_id
-                                FROM suppliers`
-export const sort = 'SELECT * FROM suppliers ORDER BY $1 $2'
-export const supplier_detail = 'SELECT * FROM suppliers WHERE id = $1'
-export const supplier_create = 'INSERT INTO suppliers (name, street, city, state, phone_number) values ($1, $2, $3, $4, $5)'
-export const supplier_update = ''
+                                FROM suppliers
+                                LIMIT $1 OFFSET $2`
+export const get_suppliers_count = `SELECT count(id) FROM suppliers`
+export const supplier_detail = `SELECT 
+                                    name,
+                                    coalesce(street, '') as street,
+                                    coalesce(city, '') as city,
+                                    coalesce(state, '') as state,
+                                    coalesce(zip::text, '') as zip,
+                                    phone_number, 
+                                    id as supplier_id
+                                FROM suppliers 
+                                WHERE id = $1`
+export const supplier_purchase_detail = `SELECT
+                                        purchases.id as purchase_name,
+                                        ARRAY(SELECT concat(products.name, ',', purchase_products.quantity, ',', products.id)
+                                                    FROM purchase_products
+                                                        INNER JOIN products ON purchase_products.product_id = products.id
+                                                    WHERE purchase_products.purchase_id = purchases.id) AS products,
+                                        to_char(purchases.purchase_date, 'MM-DD-YYYY') as purchase_date,
+                                        purchases.total_amount,
+                                        stores.store_name as store_name,
+                                        purchases.staff,
+                                        purchases.id as purchase_id
+                                    FROM purchases
+                                        INNER JOIN stores ON purchases.store = stores.id
+                                    WHERE purchases.supplier = $1`
+export const supplier_most_purchase = `SELECT products.name, sum(purchase_products.quantity) as quantity
+                                    FROM purchases
+                                        INNER JOIN purchase_products ON purchases.id = purchase_products.purchase_id
+                                        INNER JOIN products ON purchase_products.product_id = products.id
+                                    WHERE purchases.supplier = $1
+                                    GROUP BY products.name
+                                    ORDER BY quantity DESC
+                                    LIMIT 1;` 
+export const supplier_create = `INSERT INTO suppliers (
+                                    name, 
+                                    phone_number, 
+                                    street, 
+                                    city, 
+                                    state, 
+                                    zip) 
+                                VALUES ($1, $2, $3, $4, $5, $6)
+                                RETURNING id`
+export const supplier_update = `UPDATE suppliers
+                                SET name = $1,
+                                    street = $2,
+                                    city = $3,
+                                    state = $4,
+                                    zip = $5,
+                                    phone_number = $6
+                                WHERE id = $7`
+export const supplier_delete = `DELETE FROM suppliers WHERE id = $1`
