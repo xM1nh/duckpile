e37cfe1ba5fc644180930566e428883a2b902fae
Asus Laptop <minhnle311@gmail.com>
2023-07-18 13:28:28 -0700

    Implemented CORS

---
+++ server/config/corsConfig.ts
@@ -0,0 +1,19 @@
+import { CorsOptions } from "cors"
+
+const allowedOrigins = [
+    'http://localhost:3000'
+]
+
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+export default corsOptions
