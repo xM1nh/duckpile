28a4f31eb44da439bf9af53ddc3922108b32112c
Asus Laptop <minhnle311@gmail.com>
2023-07-19 14:11:02 -0700

    Setup Redux Skeleton

---
+++ client/src/app/api/apiSlice.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: 'http://localhost:3001',
+    credentials: 'include',
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token
+
+        if (token) {
+            headers.set('authorization', `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
+const apiSlice = createApi({
+    baseQuery,
+    endpoints: builder => ({})
+})
+
+export default apiSlice
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch'
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <Provider store={store}>
+      <RouteSwitch />
+    </Provider>
   </React.StrictMode>
 );
 
