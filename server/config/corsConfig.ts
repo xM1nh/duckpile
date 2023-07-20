eee25990429e6249d87fcff57c2b158c17e191a5
Asus Laptop <minhnle311@gmail.com>
2023-07-20 11:50:47 -0700

    Add origin

--- server/config/corsConfig.ts
+++ server/config/corsConfig.ts
@@ -1,7 +1,8 @@
 import { CorsOptions } from "cors"
 
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:3001'
 ]
 
 const corsOptions: CorsOptions = {
