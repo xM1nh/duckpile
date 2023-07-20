5e5a96d1f8ee8e94a60f9860849bd759f31eb624
Asus Laptop <minhnle311@gmail.com>
2023-07-20 11:50:04 -0700

    Added create get route

--- server/api/product/productRoutes.ts
+++ server/api/product/productRoutes.ts
@@ -6,6 +6,8 @@ router.get('/', product_controller.product_list)
 
 router.get('/product/:id', product_controller.product_detail)
 
+router.get('/create', product_controller.product_create_get)
+
 router.post('/create', product_controller.product_create_post)
 
 router.post('/product/:id/delete', product_controller.product_delete_post)
