d7f9a445bb58ff78a7c10678a163a8279bf29b27
Asus Laptop <minhnle311@gmail.com>
2023-07-21 14:00:12 -0700

    Implement Jwt

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:3001',
@@ -13,8 +14,35 @@ const baseQuery = fetchBaseQuery({
     }
 })
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    //console.log('args', args)
+    //console.log('api', api)
+    //console.log('extraOptions',extraOptions)
+    
+    let result = await baseQuery(args, api, extraOptions)
+
+    if (result?.error?.status === 403) {
+        console.log('sending refresh token')
+
+        const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
+
+        if (refreshResult?.data) {
+            api.dispatch(setCredentials({...refreshResult.data}))
+
+            result = await baseQuery(args, api, extraOptions)
+        } else {
+            if (refreshResult?.error?.status === 403) {
+                refreshResult.error.data.message = 'Your login has expired. '
+            }
+            return refreshResult
+        }
+    }
+
+    return result
+}
+
 const apiSlice = createApi({
-    baseQuery,
+    baseQuery: baseQueryWithReauth,
     endpoints: builder => ({})
 })
 
