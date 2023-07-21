207185c6ff7649aed28c75504bde7186d8a8fec1
Asus Laptop <minhnle311@gmail.com>
2023-07-21 14:39:30 -0700

    Add available product for each inventory

--- server/api/inventory/inventoryController.ts
+++ server/api/inventory/inventoryController.ts
@@ -4,18 +4,39 @@ import asyncHandler from 'express-async-handler'
 
 export const inventory_list = asyncHandler(async (req, res, next) => {
     const inventory = await pool.query(inventory_queries.get_all_inventory)
-    const store1 = inventory.rows.map((row :{product_name: string, inventory_store_1: number, product_id: number}) => {
-        const {product_name, inventory_store_1, product_id, ...rest} = row
-        return {product_name, quantity: inventory_store_1, product_id}
-    })
-    const store2 = inventory.rows.map((row :{product_name: string, inventory_store_2: number, product_id: number}) => {
-        const {product_name, inventory_store_2, product_id, ...rest} = row
-        return {product_name, quantity: inventory_store_2, product_id}
-    })
-    const store3 = inventory.rows.map((row :{product_name: string, inventory_store_3: number, product_id: number}) => {
-        const {product_name, inventory_store_3, product_id, ...rest} = row
-        return {product_name, quantity: inventory_store_3, product_id}
-    })
+    const store1 = {
+        availProducts: 
+            inventory.rows.filter((row :{inventory_store_1: number}) => 
+                row.inventory_store_1
+            ).length,
+        inventory: 
+                inventory.rows.map((row :{product_name: string, inventory_store_1: number, product_id: number}) => {
+                    const {product_name, inventory_store_1, product_id, ...rest} = row
+                    return {product_name, quantity: inventory_store_1, product_id}
+                })
+    }
+    const store2 = {
+        availProducts: 
+            inventory.rows.filter((row :{inventory_store_2: number}) => 
+                row.inventory_store_2
+            ).length,
+        inventory: 
+                inventory.rows.map((row :{product_name: string, inventory_store_2: number, product_id: number}) => {
+                    const {product_name, inventory_store_2, product_id, ...rest} = row
+                    return {product_name, quantity: inventory_store_2, product_id}
+                })
+    }
+    const store3 = {
+        availProducts: 
+            inventory.rows.filter((row :{inventory_store_3: number}) => 
+                row.inventory_store_3
+            ).length,
+        inventory: 
+                inventory.rows.map((row :{product_name: string, inventory_store_3: number, product_id: number}) => {
+                    const {product_name, inventory_store_3, product_id, ...rest} = row
+                    return {product_name, quantity: inventory_store_3, product_id}
+                })
+    }
 
     res.status(200).json({store1, store2, store3})
 })
